8086e311c24b148330d39f2abd2b824b2822122f
ЛатыповА <@223_9>
2018-06-11 19:02:44 +0300

    Add project files.

---
+++ RentTaxi.LIB/ServicesXML.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RentTaxi.LIB
+{
+   public class ServicesXML
+    {
+       public string path { get; set; }
+        public ServicesXML(string path)
+        {
+            this.path = path;
+        }
+
+        public void l(string path)
+        {
+
+           
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+
+
+
+            }
+        }
+
+      public XmlDocument GetDocument()
+        { 
+            XmlDocument xmlDoc = new XmlDocument();
+            //проверить есть ли документ
+            FileInfo info = new FileInfo(path);
+            //если есть загрузить и вернуть
+            if (info.Exists)
+            {
+                xmlDoc.Load(path);
+            }
+            else
+            {
+                //если нет то создать, создать корневой элемент и вернуть
+                XmlElement root = xmlDoc.CreateElement("Users");
+                xmlDoc.AppendChild(root);
+                xmlDoc.Save(path);
+
+            }
+            return xmlDoc;
+        }
+
+        public XmlElement GetUser(string login, string password)
+        {
+
+            return null;
+        }
+
+    }
+}
---
+++ RentTaxi/AdminModule/TBL_User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentTaxi.AdminModule
+{
+   public class TBL_User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Department { get; set; }
+        List<Tbl_Roles> Roles;
+
+    }
+
+    public class Tbl_Roles
+    {
+        public int RoleId { get; set; }
+        public string Names { get; set; }
+    }
+
+
+    //class UserRoles
+    //{
+    //    public int UserRoleId { get; set; }
+    //    public string Names { get; set; }
+    //}
+
+}
